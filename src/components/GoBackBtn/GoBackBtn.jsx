7addd58c4d8d440d874389e54dfc0d23f65efacb
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-08-14 21:44:48 +0300

    Initial commit

---
+++ src/components/GoBackBtn/GoBackBtn.jsx
@@ -0,0 +1,10 @@
+import s from "./GoBackBtn.module.css";
+
+const GoBackBtn = ({ path, children }) => {
+  return (
+    <NavLink className={s.link} to={path}>
+      {children}
+    </NavLink>
+  );
+};
+export default GoBackBtn;
