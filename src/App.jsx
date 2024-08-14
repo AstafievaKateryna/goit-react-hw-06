91d97a83db224c8d796af375ae02acb2085f851a
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-08-14 22:09:29 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-import { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Navigation } from "./components/Navigation/Navigation";
-import { Audio } from "react-loader-spinner";
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
-const MovieDetailsPage = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage")
-);
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
-);
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
-const App = () => {
+function App() {
   return (
-    <>
-      <Navigation />
-      <Suspense
-        fallback={
-          <Audio
-            height="80"
-            width="80"
-            radius="9"
-            color="green"
-            ariaLabel="loading"
-            wrapperStyle
-            wrapperClass
-          />
-        }
-      >
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieID" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <div className="card">
+        <ContactForm />
+        <SearchBox />
+      </div>
+      <ContactList />
+    </div>
   );
-};
+}
 
 export default App;
